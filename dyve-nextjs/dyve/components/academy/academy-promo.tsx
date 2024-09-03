5bedc643f8bd52364f0b51cddc3bf1e5a31f56c5
Obaloluwa Olusoji <olusojiobah@gmail.com>
2024-09-03 18:03:52 +0100

    fix: resolved bullet point issue

--- dyve-nextjs/dyve/components/academy/academy-promo.tsx
+++ dyve-nextjs/dyve/components/academy/academy-promo.tsx
@@ -3,25 +3,21 @@ import Link from "next/link";
 
 export function AcademyPromo() {
   return (
-    <div className="rounded-r1 flex items-center justify-between bg-[#13121D] px-20 py-10 text-[#A7A5A5]">
+    <div className="flex items-center justify-between rounded-r1 bg-[#13121D] px-20 py-10 text-[#A7A5A5]">
       <div className="w-[60%] space-y-6">
         <h3 className="text-5xl font-semibold leading-[58px]">
           Learn, Earn, and Grow with Our Academy
         </h3>
 
-        {/* //list-image-[url('images/star.svg')] */}
-
-        <ul className="space-y-3 text-sm font-medium text-[#8E8B96]">
-          <li className="">
-            Learn at Your Own Pace with Easy-to-Follow Lessons
-          </li>
+        <ul className="list-image-[url('/images/star.png')] space-y-3 text-sm font-medium text-[#8E8B96]">
+          <li>Learn at Your Own Pace with Easy-to-Follow Lessons</li>
           <li>Gain Practical Skills You Can Use Right Away</li>
           <li>Earn Verified Blockchain Certificates!</li>
         </ul>
 
         <Link
           href={"/"}
-          className="rounded-r1 inline-block bg-[#2D2D2D] px-[30px] py-[15px] text-base font-medium text-white hover:bg-purple-2"
+          className="inline-block rounded-r1 bg-[#2D2D2D] px-[30px] py-[15px] text-base font-medium text-white hover:bg-purple-2"
         >
           START TODAY!
         </Link>
