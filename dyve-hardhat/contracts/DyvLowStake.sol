8997d22fd4109d82689e3004b09ddb326c7b4071
olawalesegun <olawalesegun0@gmail>
2024-09-03 16:07:35 +0300

    Updates on staking contract and beginning of test for it

---
+++ dyve-hardhat/contracts/DyvLowStake.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+
+// import "@synthetixio/contracts/Synthetix.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DyvLowStake {
+  
+}
